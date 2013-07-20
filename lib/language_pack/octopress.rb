4275058af21d939110b504203eb57ecaa411ac63
Stefano Angaran <stefano.angaran@upyou.it>
2013-07-20 20:02:33 +0200

    Update octopress.rb

--- lib/language_pack/octopress.rb
+++ lib/language_pack/octopress.rb
@@ -23,6 +23,6 @@ class LanguagePack::Octopress < LanguagePack::Jekyll
     if File.exists?(".slugignore") && File.read(".slugignore") =~ /plugins|sass|source/
       error ".slugignore contains #{$&}. Octopress generation will fail."
     end
-    pipe("env PATH=bin:$PATH bundle exec rake generate 2>&1")
+    pipe("env PATH=$PATH:bin bundle exec rake generate 2>&1")
   end
 end
